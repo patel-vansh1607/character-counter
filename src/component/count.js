eb21e0ffbc833cf4a4800fec98f791953f2099d2
Vansh Patel <patel.vansh1607@gmail.com>
2025-04-03 09:59:48 +0300

    add styling and update character count display in CountCharacter component

--- src/component/count.js
+++ src/component/count.js
@@ -5,9 +5,9 @@ const CountCharacter = () => {
     const [character, setCharacter] = useState("")
 
     return(
-        <div>
+        <div className="main">
             <textarea className="text-count" placeholder="type something" value={character} onChange={(e) => setCharacter(e.target.value)}></textarea>
-            <h1 className={`character-count ${character.length > 100 ? "warning" : ""}`}>character = {character.length}</h1>
+            <h1 className={`character-count ${character.length > 100 ? "warning" : ""}`}>Amount of Characters = {character.length}</h1>
         </div>
     )
 }
